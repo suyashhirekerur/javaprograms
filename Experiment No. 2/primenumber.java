205bef37a9c5f99d05d4473867b3f2037d1c3b42
Suyash Hirekerur <shirekerur22@gmail.com>
2026-01-26 12:06:36 +0530

    Create primenumber.java

---
+++ Experiment No. 2/primenumber.java
@@ -0,0 +1,24 @@
+public class primenumber {
+    public static void main(String[] args) {
+
+        int num = Integer.parseInt(args[0]);
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a Prime Number");
+        } else {
+            System.out.println(num + " is not a Prime Number");
+        }
+    }
+}
